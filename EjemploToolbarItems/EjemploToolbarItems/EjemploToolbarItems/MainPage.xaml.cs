06d02b93682d361f3409155c80a72a0c0f4c7aa8
Alexiscr <canizalez_21@hotmail.com>
2019-03-11 21:22:53 -0600

    Ejemplo de uso de ToolbarItems

---
+++ EjemploToolbarItems/EjemploToolbarItems/EjemploToolbarItems/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EjemploToolbarItems
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Clic para el botón primario
+        private void BotonHola(object sender, EventArgs e)
+        {
+            DisplayAlert("Mensaje","Hola Mundo","Ok");
+        }
+    }
+}
